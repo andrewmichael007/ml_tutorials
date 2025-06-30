063b5631cf8034643cf202604d36fcb5262f69ba
Andy-Techy <adjeimitchel03@gmail.com>
2025-06-30 23:59:01 +0000

    added a file

--- linearRegression/intuitionII.py
+++ linearRegression/intuitionII.py
@@ -3,20 +3,21 @@
 
 
 # GRADIENT DESCENT INTUITION
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Data: shoe sizes and heights
 shoe_sizes = np.array([7, 8, 9, 10, 11])  # feature (x)
-heights = np.array([160, 165, 170, 175, 180])  # target (y))
+heights = np.array([160, 165, 170, 175, 180])  # target (y)
 m = len(heights)  # Number of  training set
 
 # setting w and b to initial values or initialize w (slope) and b (intercept) randomly
-w = 4.51 # np.random.rand()
-b = 128  # np.random.rand()
+w = np.random.rand()
+b = np.random.rand()
 
 # Learning rate (step size)
-alpha = 0.000009
+alpha = 0.000002130
 number_of_iterations = 1000  # Number of iterations
 
 # store cost history for plotting
@@ -26,27 +27,26 @@ cost_history = []
 for each in range(number_of_iterations):
     # compute prediction for each value of w, b for y_hat
     y_hat = w * shoe_sizes + b
-    
+
     # compute error
     errors = np.sum(y_hat - heights)
 
-    # this block of code computes the mean squareed error
-
-    squared_error  = (errors ** 2)# for squarred error
+    # this block of code computes the mean squared error
+    squared_error  = (errors ** 2) # for squared error
     cost_function =  (1 / (2 * m)) * (squared_error) # final cost function calculation
     cost_history.append(cost_function)
 
     # the next block of code updates the w and b parameters automatically
     # the algorithm;
-    # for w; 
+    # for w;
     # 1. take the initial value of  w
     # 2. compute their derivative, how ? just multipy  1/m by the sum of the errors
     # 3. now multiply the derivative by alpha
     # 4. subtract the total from the w
     # 5. total is the new value  of w
 
-    derivative_of_w = (1 / m) * np.sum(errors * shoe_sizes)  
-    derivative_of_b = (1 / m) * np.sum(errors) 
+    derivative_of_w = (1 / m) * np.sum(errors * shoe_sizes)
+    derivative_of_b = (1 / m) * np.sum(errors)
 
     # updating the  parameters of w and b
     w -= alpha * derivative_of_w
@@ -80,4 +80,10 @@ plt.legend()
 plt.title("Final Model Fit")
 plt.show()
 
+def predict_height(shoe_size, w, b):
+    return w * shoe_size + b
 
+# Example usage
+shoe_size = 12
+predicted_height = predict_height(shoe_size, w, b)
+print(f"The predicted height for a shoe size of {shoe_size} is {predicted_height:.2f} cm")
